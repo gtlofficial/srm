4a5aab368fb76aa8831af62d97a16b1369b30992
GTL WEB SOLUTIONS <155606177+gtlofficial@users.noreply.github.com>
2026-01-05 12:49:57 +0530

    SRM

--- components/common/Accordion.jsx
+++ components/common/Accordion.jsx
@@ -56,7 +56,7 @@ export default function Accordion({
             {item.question}
           </a>
           <div
-            className="uc-accordion-content"
+            className="uc-accordion-content text-start"
             ref={(el) => (answerRefs.current[index] = el)}
           >
             <p>{item.answer}</p>

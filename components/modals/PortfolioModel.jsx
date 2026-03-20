2be3a16b309b659f905a55a73440c72170df71c0
GTL WEB SOLUTIONS <155606177+gtlofficial@users.noreply.github.com>
2026-03-20 15:39:10 +0530

    Notification

--- components/modals/PortfolioModel.jsx
+++ components/modals/PortfolioModel.jsx
@@ -1,5 +1,5 @@
 "use client";
- 
+import { closePortfolioModal } from "@/utlis/togglePortfolioModel";
 import { usePathname } from "next/navigation";
 import { useEffect, useRef } from "react";
 
@@ -37,7 +37,31 @@ export default function PortfolioModal({ project }) {
       style={{ display: "block" }}
       tabIndex={-1}
     >
-       
+      <div
+        ref={modalRef}
+        className="d-flex uc-modal-dialog max-w-1000px w-md-100 bg-white dark:bg-gray-900 rounded-1 position-relative"
+        role="dialog"
+        aria-modal="true"
+      >
+        <button
+          className="uc-modal-close-default p-0 icon-3 btn border-0 position-absolute top-0 end-0 m-2 dark:text-white dark:text-opacity-50 hover:text-primary hover:rotate-90 duration-150 transition-all"
+          type="button"
+          onClick={closePortfolioModal}
+        >
+          <i className="unicon-close" />
+        </button>
+
+        {project && (
+          <div className="d-flex items-center justify-content-center">
+            <img
+              src={project.image}
+              alt={project.title}
+              className="img-fluid"
+              style={{objectFit: "contain",}}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }

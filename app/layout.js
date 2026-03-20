51ecf354f1b414f7ddb2605fe55eb3d1a3e7d624
GTL WEB SOLUTIONS <155606177+gtlofficial@users.noreply.github.com>
2026-03-20 15:35:12 +0530

    Notification

--- app/layout.js
+++ app/layout.js
@@ -9,10 +9,15 @@ import "swiper/css/effect-fade";
 import "../public/assets/custom.scss";
 
 import { useEffect } from "react";
- 
- 
-import { ParallaxProvider } from "react-scroll-parallax";  
- 
+import MobileMenu from "@/components/headers/component/MobileMenu";
+import BacktoTop from "@/components/common/BacktoTop";
+import { ParallaxProvider } from "react-scroll-parallax";
+import ContactModal from "@/components/modals/ContactModal";
+import NewsletterModal from "@/components/modals/NewsletterModal";
+import SearchModal from "@/components/modals/SearchModal";
+import { Toaster } from "react-hot-toast"; // ✅ Toast added
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }) {
   useEffect(() => {
@@ -36,13 +41,43 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" dir="ltr">
       <body>
-  
+        {/* ✅ reCAPTCHA script here */}
+        <Script
+          src="https://www.google.com/recaptcha/api.js?render=explicit"
+          strategy="beforeInteractive"
+          async
+          defer
+        />
         <Context>
           <ParallaxProvider>{children}</ParallaxProvider>
-    
-     
+          <MobileMenu />
+          <ContactModal />
+          <NewsletterModal />
+          <SearchModal />
+          <BacktoTop />
+          <SpeedInsights />
+          <Analytics />
         </Context>
-      
+        <Toaster
+          toastOptions={{
+            duration: 3000,
+            style: {
+              background: "rgba(0, 0, 0, 0.85)",
+              color: "#fff",
+              fontSize: "16px",
+              padding: "16px 20px",
+              borderRadius: "12px",
+              maxWidth: "100%",
+            },
+          }}
+          containerStyle={{
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            position: "fixed",
+            zIndex: 9999,
+          }}
+        />
       </body>
     </html>
   );

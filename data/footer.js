8fe816281b1418a082cd449ef10d03a1484be832
GTL WEB SOLUTIONS <155606177+gtlofficial@users.noreply.github.com>
2026-03-20 10:59:51 +0530

    Notification

--- data/footer.js
+++ data/footer.js
@@ -1,25 +1,2 @@
-export const footerLinks = [
-  {
-    title: "Company",
-    links: [
-      { href: "", label: "Company" },
-      { href: "/page-about", label: "About us" },
-      { href: "/page-features", label: "Services" },
-      { href: "/page-faqs", label: "Faq's" },
-      { href: "/page-contact", label: "Contact" },
-    ],
-  },
-  {
-    title: "Social",
-    links: [
-      { href: "", label: "Social" },
-      { href: "https://www.facebook.com/srm.vehiclerepaircentre/", target: "_blank", label: "Facebook" },
-      { href: "https://www.instagram.com/srm.vehiclerepaircentre/", target: "_blank", label: "Instagram" },
-    ],
-  },
-];
-
-export const socialLinks = [
-  { href: "https://www.facebook.com/srm.vehiclerepaircentre/", iconClass: "unicon-logo-facebook", label: "Facebook" },
-  { href: "https://www.instagram.com/srm.vehiclerepaircentre/", iconClass: "unicon-logo-instagram", label: "Instagram" },
-];
+export const footerLinks = [];
+export const socialLinks = [];

0c316af535e18fd599cf36aa11e9534274e1392e
GTL WEB SOLUTIONS <155606177+gtlofficial@users.noreply.github.com>
2026-03-20 15:38:21 +0530

    Notification

--- components/modals/SearchModal.jsx
+++ components/modals/SearchModal.jsx
@@ -1,5 +1,6 @@
 "use client";
- 
+
+import { closeSearchModal } from "@/utlis/toggleSearchModal";
 import { usePathname } from "next/navigation";
 import { useEffect, useRef } from "react";
 
@@ -39,7 +40,43 @@ export default function SearchModal() {
       style={{ display: "block" }}
       tabIndex={-1}
     >
-       
+      <div
+        ref={elementRef}
+        className="uc-modal-dialog d-flex justify-center bg-white text-dark dark:bg-gray-900 dark:text-white"
+        data-uc-height-viewport=""
+        role="dialog"
+        aria-modal="true"
+        style={{ minHeight: "auto" }}
+      >
+        <div className="uc-modal-close-full m-1 p-0 vstack gap-narrow text-center">
+          <button
+            className="icon-3 btn btn-md btn-dark dark:bg-white dark:text-dark w-24px sm:w-32px h-24px sm:h-32px rounded-circle flex-1"
+            type="button"
+            onClick={closeSearchModal}
+          >
+            <i className="unicon-close" />
+          </button>
+        </div>
+        <div className="panel w-100 sm:w-500px px-2 py-10">
+          <h3 className="h4 sm:h2 text-center">What are you looking for?</h3>
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            className="hstack gap-1 mt-4 border-bottom p-narrow dark:border-gray-700"
+          >
+            <span className="d-inline-flex justify-center items-center w-24px sm:w-40 h-24px sm:h-40px opacity-50">
+              <i className="unicon-search icon-3" />
+            </span>
+            <input
+              type="search"
+              name="q"
+              className="form-control-plaintext ms-1 fs-6 sm:fs-5 w-full dark:text-white"
+              placeholder="Type your keyword.."
+              aria-label="Search"
+              autoFocus
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
